a379aaf86c0f8ba157a981cda6497d62d74c4270
kouwasi <kouwasi@gmail.com>
2018-05-26 19:32:29 +0900

    ..

--- app/index.js
+++ app/index.js
@@ -7,18 +7,47 @@ client.on('ready', () => {
   console.log("Ready!")
 })
 
-client.on('message', (msg) => {
+client.on('message', async (msg) => {
   if(msg.author.id == client.user.id) return
 
   if(msg.content === '!!') {
-    request(`https://api.giphy.com/v1/gifs/random?api_key=${config.giphy.token}&tag=nichijou&rating=R`, (error, response, body) => {
-      let jsonBody = JSON.parse(body);
+    if(getRandomInt(2) == 0) {
+      url = await getUrlFromTenor().catch(console.error)
+    } else {
+      url = await getUrlFromGiphy().catch(console.error)
+    }
 
-      msg.channel.send(jsonBody.data.url)
-      .catch(console.error)
-    })
-
+    msg.channel.send(url)
+    .catch(console.error)
   }
 })
 
-client.login(config.token)
+function getUrlFromGiphy() {
+  return new Promise((resolve, reject ) => {
+    request(`https://api.giphy.com/v1/gifs/random?api_key=${config.giphy.token}&tag=nichijou&rating=R`, (error, response, body) => {
+      if(!error && response.statusCode == 200) {
+        resolve(JSON.parse(body).data.url)
+      } else {
+        reject(error)
+      }
+    })
+  })
+}
+
+function getUrlFromTenor() {
+  return new Promise((resolve, reject ) => {
+    request(`https://api.tenor.com/v1/random?key=${config.tenor.token}&q=nichijou&safesearch=moderate&limit=1`, (error, response, body) => {
+      if(!error && response.statusCode == 200) {
+        resolve(JSON.parse(body).results[0].url)
+      } else {
+        reject(error)
+      }
+    })
+  })
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+client.login(config.discord.token)
